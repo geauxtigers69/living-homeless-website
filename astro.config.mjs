bd5af8f26e41824c04f3d73167d2bc9b583def6a
geauxtigers69 <freestackdeveloper@gmail.com>
2023-11-13 09:46:02 -0500

    update astro config with deploy integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://living-homeless.deno.dev',
-  integrations: [mdx(), sitemap(), tailwind(), svelte()]
+  site: 'https://living-homeless.vercel.app',
+  integrations: [mdx(), sitemap(), tailwind(), svelte()],
+  output: "server",
+  adapter: vercel()
 });
