2c462433b5240c8f415d8687c7c21f2d3a68014d
geauxtigers69 <freestackdeveloper@gmail.com>
2023-11-13 09:49:04 -0500

    update changes for deploying

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://living-homeless.vercel.app',
   integrations: [mdx(), sitemap(), tailwind(), svelte()],
   output: "server",
-  adapter: vercel()
+  adapter: netlify()
 });
